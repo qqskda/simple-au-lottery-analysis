33b28804e2b021e7f79a9c832d99dd747d2b1061
Sungkwon Lee <lee.sk.qq@gmail.com>
2021-02-07 02:08:41 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Austrlian Lottery Ticket Generator
+ 
---
+++ lottery.py
@@ -0,0 +1,7 @@
+import csv
+with open('Wednesday Lotto Results - G NETWORK.csv', 'r') as file:
+    wedLotto = csv.reader(file)
+
+with open('Saturday Lotto Results - G NETWORK.csv', 'r') as file:
+    satlooto = csv.reader(file)
+
