0e37540d18740856f3f742bcb7d56d00d98795d6
Sungkwon Lee <lee.sk.qq@gmail.com>
2021-03-20 00:43:53 +1100

    temp

--- lottery.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('Wednesday Lotto Results - G NETWORK.csv', 'r') as file:
-    wedLotto = csv.reader(file)
-
-with open('Saturday Lotto Results - G NETWORK.csv', 'r') as file:
-    satlooto = csv.reader(file)
-
